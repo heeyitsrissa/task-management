2a0cab391e07cbb07d4787046881a164b1441352
Marissa Pratt <131078835+heeyitsrissa@users.noreply.github.com>
2024-04-04 22:10:23 -0400

    fixed re-register error handler

--- controllers/api/registerRoute.js
+++ controllers/api/registerRoute.js
@@ -2,7 +2,17 @@ const { User } = require('../../models');
 
 exports.registerUser = async (req, res) => {
     try {
-        const userData = await User.create(req.body);
+           
+        const existingUser = await User.findOne({ where: { email: req.body.email } });
+            if (existingUser) {
+
+                return res.status(401).render('error401', {
+                    message: 'Email is already registered either register with new email or log in'
+                });
+            }
+
+
+         const userData = await User.create(req.body);
 
         req.session.save(() => {
             req.session.user_id = userData.id;
